24ff80750c78743bc58f4bf8e05859c577954044
Yurii Hriaziev <yuriihriaziev@Yuriis-Laptop.attlocal.net>
2024-09-19 23:29:13 -0400

    Update to the list of countries, currently displaying 180 of 193 countries

--- backend/main.py
+++ backend/main.py
@@ -14,20 +14,17 @@ def create():
 
 @app.route("/get_country_list")
 def get_country_list():
-    api_url = 'https://restcountries.com/v3.1/all'
-
+    '''
+        Create a map of long:short country names
+        Use the map to rename .shp 'NAME' column with the long version
+    '''
     try:
-        response = requests.get(api_url)
-        response.raise_for_status()
-
-        country_data = response.json()
-        list_of_names = []
-        for country in country_data:
-            list_of_names.append(country.get('name', {}).get('common', 'Undefined'))
-
-        return jsonify({'names':list_of_names})
+        country_map = generate_country_list()
+        country_list = list(country_map.keys())
+        return jsonify({'names':country_list})
     except Exception as e:
         return jsonify({'message':f'{e}'}), 400
+    
 
 @app.route("/population", methods=['POST'])
 def population():
@@ -95,20 +92,48 @@ def population():
         return jsonify({'message':f'{e}'}), 400
     
 
+def generate_country_list():
+    countries_path = 'static/countries.txt'
+    with open(countries_path, 'r') as file:
+        txt_names = file.read().splitlines()
+
+    shp_path = 'static/ne_10m_admin_0_countries.shp'
+    gdf = gpd.read_file(shp_path)
+    shp_names = []
+    if 'NAME' in gdf.columns:
+        shp_names = gdf['NAME'].unique()
+
+    shp_names.sort()
+
+    country_map = {}
+    for i in range(len(txt_names)):
+        for j in range(len(shp_names)):
+            if len(txt_names[i]) >= 10:
+                if txt_names[i][:10] in shp_names[j]:
+                    country_map[txt_names[i]] = shp_names[j]
+            else:
+                if txt_names[i] == shp_names[j]:
+                    country_map[txt_names[i]] = shp_names[j]
+
+    return country_map
+
 '''
     Takes in 'country name' as parameter to return that country's geo json data
     Uses data from 'Natural Earth'
 '''
 def static_bounds(country_name):
     try:
-
         if country_name == 'Ukraine':
             shp_path = 'static/ukraine-with-crimea_1691.geojson'
             country = gpd.read_file(shp_path)
         else:
+            country_map = generate_country_list()
+            shp_name = country_map[country_name]
             shp_path = 'static/ne_10m_admin_0_countries.shp'
             gdf = gpd.read_file(shp_path)
-            country = gdf[gdf['NAME'] == country_name]
+            if 'NAME' in gdf.columns:
+                print(gdf['NAME'].unique())
+            country = gdf[gdf['NAME'] == shp_name]
 
         if country.empty:
             return jsonify({'message':'Country not found'})
